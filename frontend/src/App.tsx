212442b11c18386f60f047c6970595bd452e8e91
octanym <camjmoore@outlook.com>
2023-05-02 10:11:22 -0400

    typed app interface and setup react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-function App() {
+export interface IApplicationProps {}
+
+const App: React.FC<IApplicationProps> = (props) => {
   return (
     <div className="bg-gray min-h-full">
       <Header />
       <main className="bg-white mx-auto min-h-screen max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-          Welcome to the Store
-        </h1>
+        <Routes>
+          <Route />
+        </Routes>
       </main>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
